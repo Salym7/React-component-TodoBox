f9a7d34df8deb3f94f706ceb44aa97ee8b0e7201
Oleksandr Solianyk <salym3k@gmail.com>
2023-01-24 18:53:05 +0200

    init react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TodoBox from "./component/TodoBox";
+
+function App() {
+    return (
+        <div className="App">
+            <TodoBox/>
+        </div>
+    );
+}
+
+export default App;
